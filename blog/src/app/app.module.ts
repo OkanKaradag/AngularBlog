276eaa3a13b43a8dbc1614b00bdbbaa98a871f52
Okan Karadağ <okan.karadag@d-teknoloji.com.tr>
2020-12-02 01:17:41 +0300

    Add about-me component

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
