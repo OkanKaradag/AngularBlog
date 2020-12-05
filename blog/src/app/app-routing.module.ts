10abe4ebe64531ea0e61aad9a45f0ad08718a05c
Okan Karadağ <okan.karadag@d-teknoloji.com.tr>
2020-12-05 17:55:40 +0300

    Add routing structure

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,7 +1,51 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+const routes: Routes =
+[
+  //www.blabla.com/
+  {
+    path : "",
+    component : MainLayoutComponent,
+    // The routing structure of the components in the main layout
+    children :
+    [
+      //www.blabla.com/ -> Main component in the layout
+      {
+        path : "",
+        component : HomeComponent
+      },
+      //www.blabla.com/about-me
+      {
+        path : "about-me",
+        component : AboutMeComponent
+      },
+      //www.blabla.com/contact
+      {
+        path : "contact",
+        component : ContactComponent
+      },
+    ]
+  },
+
+  //www.blabla.com/admin
+  {
+    path : "admin",
+    component : AdminLayoutComponent,
+    children :
+    [
+      //TODO:Write the admin component's routings
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
