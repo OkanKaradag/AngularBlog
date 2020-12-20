7bf0c1c8b387352c1f83052d98282f3b54b180b6
Okan Karadağ <okan.karadag@d-teknoloji.com.tr>
2020-12-21 00:52:45 +0300

    Add menu-category component

---
+++ blog/src/app/components/menu-category/menu-category.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+import {CategoryService} from '../../services/category.service'
+
+@Component({
+  selector: 'app-menu-category',
+  templateUrl: './menu-category.component.html',
+  styleUrls: ['./menu-category.component.css']
+})
+export class MenuCategoryComponent implements OnInit {
+  categories: Category[] = [];
+  constructor(private categoryService: CategoryService) { }
+
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe(data => {
+      this.categories = data;
+    });
+  }
+
+}
