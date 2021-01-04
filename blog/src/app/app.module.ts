22b265b58a9b0e288c8b90e49363a673386de9d7
Okan <o.karadag@outlook.com>
2021-01-05 00:50:22 +0300

    Simplify app module

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -3,36 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { PagesModule } from './pages/pages.module';
 
 // Components
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutMeComponent } from './pages/about-me/about-me.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
-import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
-import { MainNavigationComponent } from './navigation/main-navigation/main-navigation.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { AdminNavigationComponent } from './navigation/admin-navigation/admin-navigation.component';
-import { MenuCategoryComponent } from './components/menu-category/menu-category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutMeComponent,
-    ContactComponent,
-    MainLayoutComponent,
     AdminLayoutComponent,
-    MainNavigationComponent,
     AdminNavigationComponent,
-    MenuCategoryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
