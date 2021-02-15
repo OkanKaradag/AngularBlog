030fbff96cd2c57ccf4fc71f07b81e81d99281c3
Okan <o.karadag@outlook.com>
2021-02-16 00:14:42 +0300

    Add title input to page-title component

--- blog/src/app/components/page-title/page-title.component.ts
+++ blog/src/app/components/page-title/page-title.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-title',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-title.component.css']
 })
 export class PageTitleComponent implements OnInit {
+@Input() title:string
 
   constructor() { }
 
