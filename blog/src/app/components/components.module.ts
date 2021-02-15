bfa1fce7c1feb604781539f96d77f0d405e1d82a
Okan <o.karadag@outlook.com>
2021-02-16 00:09:51 +0300

    Add page-title component

--- blog/src/app/components/components.module.ts
+++ blog/src/app/components/components.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MenuCategoryComponent } from './menu-category/menu-category.component';
+import { PageTitleComponent } from './page-title/page-title.component';
 
 @NgModule({
-  declarations: [MenuCategoryComponent],
+  declarations: [MenuCategoryComponent, PageTitleComponent],
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports: [MenuCategoryComponent]
+  exports: [MenuCategoryComponent,PageTitleComponent]
 })
 export class ComponentsModule { }
