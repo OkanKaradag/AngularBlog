511cfcce8b0a21457c5c135411cb500f0956d77a
Okan <o.karadag@outlook.com>
2021-05-16 15:55:40 +0300

    Add CategoryResponse model

---
+++ AngularBlogCore.API/AngularBlogCore.API/Models/ResponseModels/CategoryResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace AngularBlogCore.API.Models.ResponseModels
+{
+    public class CategoryResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
