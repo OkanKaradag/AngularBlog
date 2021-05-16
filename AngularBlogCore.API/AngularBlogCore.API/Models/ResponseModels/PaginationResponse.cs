8a7bc18fb1d030945813c3cb0b2889fb7d84d5be
Okan <o.karadag@outlook.com>
2021-05-16 15:57:11 +0300

    Add PaginationResponse model

---
+++ AngularBlogCore.API/AngularBlogCore.API/Models/ResponseModels/PaginationResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularBlogCore.API.Models.ResponseModels
+{
+    public class PaginationResponse<T>
+    {
+        public int TotalCount { get; set; }
+        public IEnumerable<T> Response { get; set; }
+    }
+}
