5710c8a97a91e5470adbf62f8735517e59e7bcc4
Okan <o.karadag@outlook.com>
2021-05-16 21:57:52 +0300

    Add pagination response model

---
+++ blog/src/app/models/pagination-response.spec.ts
@@ -0,0 +1,7 @@
+import { PaginationResponse } from './pagination-response';
+
+describe('PaginationResponse', () => {
+  it('should create an instance', () => {
+    expect(new PaginationResponse()).toBeTruthy();
+  });
+});
---
+++ blog/src/app/models/pagination-response.ts
@@ -0,0 +1,4 @@
+export class PaginationResponse<T> {
+  totalCount: number;
+  response: T[];
+}
