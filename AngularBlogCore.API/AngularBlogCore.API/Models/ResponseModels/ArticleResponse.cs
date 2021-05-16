c0778433321669ff95189a96b7268a87f2ba9047
Okan <o.karadag@outlook.com>
2021-05-16 15:56:54 +0300

    Add ArticleResponse model

---
+++ AngularBlogCore.API/AngularBlogCore.API/Models/ResponseModels/ArticleResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AngularBlogCore.API.Models.ResponseModels
+{
+    public class ArticleResponse
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ContentMain { get; set; }
+        public string ContentSummary { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string Picture { get; set; }
+        public int ViewCount { get; set; }
+        public int CommentCount { get; set; }
+
+        public CategoryResponse Category { get; set; }
+    }
+}
