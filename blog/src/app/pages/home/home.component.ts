c83edafc7deaf14e6fcdffcb6186c7a8b9b439f2
Okan <o.karadag@outlook.com>
2021-06-18 02:33:41 +0300

    Call article service according to pagination path on home page

--- blog/src/app/pages/home/home.component.ts
+++ blog/src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from 'src/app/models/article';
+import { ArticleService } from 'src/app/services/article.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  page = 1;
+  articles: Article[];
+  totalCount = 0;
+  pageSize = 5;
 
-  ngOnInit(): void {}
+  constructor(
+    private articleService: ArticleService,
+    private activatedRout: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRout.paramMap.subscribe((params) => {
+      var page = params.get('page');
+
+      if (page) this.page = Number(page);
+
+      this.articles = [];
+      this.totalCount = 0;
+
+      this.articleService
+        .getArticles(this.page, this.pageSize)
+        .subscribe((data) => {
+          this.articles = data.response;
+          this.totalCount = data.totalCount;
+        });
+    });
+  }
 }
