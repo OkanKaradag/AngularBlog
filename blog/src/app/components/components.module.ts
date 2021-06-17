842c85891d26f64a6f3b5b143347a783a905096a
Okan <o.karadag@outlook.com>
2021-06-18 02:47:10 +0300

    Add ngx-pagination library

--- blog/src/app/components/components.module.ts
+++ blog/src/app/components/components.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { NgxPaginationModule } from 'ngx-pagination';
+
 import { MenuCategoryComponent } from './menu-category/menu-category.component';
 import { PageTitleComponent } from './page-title/page-title.component';
 
 @NgModule({
   declarations: [MenuCategoryComponent, PageTitleComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NgxPaginationModule],
   exports: [MenuCategoryComponent, PageTitleComponent],
 })
 export class ComponentsModule {}
